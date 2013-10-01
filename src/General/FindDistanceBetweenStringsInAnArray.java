546f90b5edda9a0a460b79162c020107ddc049a2
Sowmya Hariharan <scarlet.m7@gmail.com>
2013-10-01 16:23:12 -0500

    Corrects the globalMin assignment value.

--- src/General/FindDistanceBetweenStringsInAnArray.java
+++ src/General/FindDistanceBetweenStringsInAnArray.java
@@ -29,7 +29,7 @@ public class FindDistanceBetweenStringsInAnArray {
     public static int findDistanceBetweenTwoStrings(ArrayList<String> array, String string1, String string2){
         StringEnclosure s1 = new StringEnclosure(string1);
         StringEnclosure s2 = new StringEnclosure(string2);
-        fillInTheIndexes(s1,s2,array);
+        fillInTheIndexes(s1, s2, array);
         return findMinimumDistance(s1.indexes, s2.indexes);
     }
     public static void fillInTheIndexes(StringEnclosure s1, StringEnclosure s2, ArrayList<String> arrayList){
@@ -47,11 +47,11 @@ public class FindDistanceBetweenStringsInAnArray {
 
     public static int findMinimumDistance(ArrayList<Integer> a1, ArrayList<Integer> a2){
         int counter1 = 0, counter2 = 0;
-        int globalMin = -1;
-        while (counter1<a1.size() && counter2 < a2.size()){
-            int min = Math.abs(a1.get(counter1) - a2.get(counter2));
-            if( globalMin < min ){
-               globalMin = min;
+        int globalMin = Integer.MAX_VALUE;
+        while(counter1<a1.size() && counter2<a2.size()){
+            int min = Math.abs(a1.get(counter1)-a2.get(counter2));
+            if(min < globalMin){
+                globalMin = min;
             }
             if(a1.get(counter1) < a2.get(counter2)){
                 ++counter1;
